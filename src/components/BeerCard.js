0dc1dc8bc24580a1a4904e1b33892d94e15090c5
jozef-wolf <jozefwolf@o2.pl>
2021-02-10 15:00:42 +0100

    BeerCard first structure

---
+++ src/components/BeerCard.js
@@ -0,0 +1,20 @@
+const BeerCard = () => {
+  return (
+    <div>
+      <div>
+        <h1>nazwa</h1>
+        <h2>producent</h2>
+        <p>informacje</p>
+        <ul>
+          <li>rodzaj piwa</li>
+          <li>alkohol</li>
+          <li>procent</li>
+          <li>ekstrakt</li>
+        </ul>
+      </div>
+      <div>
+        <img></img>
+      </div>
+    </div>
+  );
+};
