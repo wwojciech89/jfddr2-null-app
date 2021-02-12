effe9ce7aff99966911f15fe28373b699a7ea756
jozef-wolf <jozefwolf@o2.pl>
2021-02-12 19:58:35 +0100

    BeerCard structure wip

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -12,23 +12,53 @@ const BeerCard = ({
   ingredients,
 }) => {
   return (
-    <div>
+    <div className="BeerCard">
       <div>
-        <img alt="picture">{picture}</img>
+        <img alt="picture" scr={picture}></img>
       </div>
       <div>
-        <h1>name{name}</h1>
-        <h2>brewery{brewery}</h2>
-        <p>description{description}</p>
-        <ul>
-          <li>type{type}</li>
-          <li>alcohol{alcohol}</li>
-          <li>origin{origin}</li>
-          <li>extract{extract}</li>
-          <li>volume{volume}</li>
-          <li>bitterness{bitterness}</li>
-          <li>ingredients{ingredients}</li>
-        </ul>
+        <h1>Crazy Mike{name}</h1>
+        <h2>AleBrowar{brewery}</h2>
+        <p>
+          <strong>Informacje:</strong>
+          {description}"Miks ananasa, pomarańczy i mango plus zestaw ultra
+          aromatycznych gatunków chmielu. Więcej nie trzeba – łap butelkę, pij
+          na chłodno i poczuj się jak w raju!"
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>Rodzaj:</td>
+              <td>{type}IPA</td>
+            </tr>
+            <tr>
+              <td>Alkohol%:</td>
+              <td>{alcohol}8</td>
+            </tr>
+            <tr>
+              <td>Pochodzenie:</td>
+              <td>{origin}Polska</td>
+            </tr>
+            <tr>
+              <td>Ekstrakt:</td>
+              <td>{extract}20</td>
+            </tr>
+            <tr>
+              <td>Pojemność:</td>
+              <td>{volume}500</td>
+            </tr>
+            <tr>
+              <td>Gorycz:</td>
+              <td>{bitterness}</td>
+            </tr>
+          </tbody>
+        </table>
+        <p>
+          <strong>Składniki:</strong>
+          {ingredients}woda; słody: Pale Ale, Pszeniczny, Wiedeński, Carapils,
+          Zakwaszający; chmiel: Cascade, Citra, Chinook, Centennial, Columbus;
+          dodatki: Candi White Sugar; drożdże: US-05.
+        </p>
       </div>
     </div>
   );
