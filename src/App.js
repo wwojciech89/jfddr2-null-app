19abe3cd7f75fdb9a7b91e9a809ceabeee28ae71
jozef-wolf <jozefwolf@o2.pl>
2021-02-14 15:04:55 +0100

    added button, responsive, no img

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import BeerCard from "./components/BeerCard";
+import "./components/BeerCard.css";
 function App() {
   return (
     <div className="App">
       <h1>piwko</h1>
+      <BeerCard />
     </div>
   );
 }
