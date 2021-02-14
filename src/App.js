e917a3d20e23eed347ad9cf2c4299b82f75d8a24
jozef-wolf <jozefwolf@o2.pl>
2021-02-14 15:11:31 +0100

    commit before with clear App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import BeerCard from "./components/BeerCard";
-import "./components/BeerCard.css";
+
 function App() {
   return (
     <div className="App">
       <h1>piwko</h1>
-      <BeerCard />
     </div>
   );
 }
