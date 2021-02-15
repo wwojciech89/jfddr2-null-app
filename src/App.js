fd7586f392dc79255f162b3da880b85ae654bee2
Tomasz Kopec <tomasz@artsound.pl>
2021-02-15 08:03:56 +0100

    15.02 commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import BeerCard from "./components/BeerCard";
 
 function App() {
   return (
     <div className="App">
+      <BeerCard />
       <h1>piwko</h1>
     </div>
   );
