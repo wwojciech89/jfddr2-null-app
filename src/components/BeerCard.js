f2a74a32776b334ff35cf2b706b872476934d4d6
Tomasz Kopec <tomasz@artsound.pl>
2021-02-15 13:41:12 +0100

    feature 10 and 12

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -1,67 +1,76 @@
 import React from "react";
+import "./BeerCard.css";
 
-const BeerCard = ({
-  name,
-  brewery,
-  description,
-  type,
-  alcohol,
-  origin,
-  extract,
-  volume,
-  picture,
-  bitterness,
-  ingredients,
-}) => {
+import { useParams } from "react-router-dom";
+
+const BeerCard = ({ beers }) => {
+  const { beerName } = useParams();
+  const foundBeer = beers.find((beer) => beer.name === beerName);
+  const { name, type, foto } = foundBeer;
+
   return (
     <div className="BeerCard">
       <div className="left">
-        <button>POWRÓT</button>
-        <img alt="picture" scr={picture}></img>
+        {/* <button>POWRÓT</button> */}
+        <img alt="beer picture" scr={foto} />
       </div>
       <div className="right">
         <div className="product_name">
-          <h1>Crazy Mike{name}</h1>
-          <h2>AleBrowar{brewery}</h2>
+          <h1>{name}</h1>
+          <h2>
+            AleBrowar
+            {/* {brewery} */}
+          </h2>
         </div>
         <div className="product_info">
           <p>
             <strong>Informacje:</strong>
-            {description}"Miks ananasa, pomarańczy i mango plus zestaw ultra
-            aromatycznych gatunków chmielu. Więcej nie trzeba – łap butelkę, pij
-            na chłodno i poczuj się jak w raju!"
+            {/* {description} */}
+            "Miks ananasa, pomarańczy i mango plus zestaw ultra aromatycznych
+            gatunków chmielu. Więcej nie trzeba – łap butelkę, pij na chłodno i
+            poczuj się jak w raju!"
           </p>
           <table>
             <tbody>
               <tr>
                 <td>Rodzaj:</td>
-                <td>{type}IPA</td>
+                <td>{type}</td>
               </tr>
               <tr>
                 <td>Alkohol%:</td>
-                <td>{alcohol}8</td>
+                <td>{/* {alcohol} */}8</td>
               </tr>
               <tr>
                 <td>Pochodzenie:</td>
-                <td>{origin}Polska</td>
+                <td>
+                  {/* {origin} */}
+                  Polska
+                </td>
               </tr>
               <tr>
                 <td>Ekstrakt:</td>
-                <td>{extract}20</td>
+                <td>
+                  {/* {extract} */}
+                  20
+                </td>
               </tr>
               <tr>
                 <td>Pojemność:</td>
-                <td>{volume}500</td>
+                <td>
+                  {/* {volume} */}
+                  500
+                </td>
               </tr>
               <tr>
                 <td>Gorycz:</td>
-                <td>{bitterness}</td>
+                {/* <td>{bitterness}</td> */}
               </tr>
             </tbody>
           </table>
           <p>
             <strong>Składniki:</strong>
-            {ingredients}woda; słody: Pale Ale, Pszeniczny, Wiedeński, Carapils,
+            {/* {ingredients} */}
+            woda; słody: Pale Ale, Pszeniczny, Wiedeński, Carapils,
             Zakwaszający; chmiel: Cascade, Citra, Chinook, Centennial, Columbus;
             dodatki: Candi White Sugar; drożdże: US-05.
           </p>
