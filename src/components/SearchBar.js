e40d10546be0c91a7277e427e448b2540a64b445
jozef-wolf <jozefwolf@o2.pl>
2021-02-17 19:57:44 +0100

    useState added to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 const SearchBar = () => {
+  const [search, setSearch] = useState("");
   return (
     <div>
-      <input type="text" placeholder="wyszukaj piwo..." />
+      <input
+        type="text"
+        placeholder="wyszukaj piwo..."
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
     </div>
   );
 };
