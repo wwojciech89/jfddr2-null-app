dc6531a1f87102e54e32e7a4283542b089e387e6
jozef-wolf <jozefwolf@o2.pl>
2021-02-22 17:54:05 +0100

    BeerCard comments and add rate/comment WIP

---
+++ src/components/AddComment.js
@@ -0,0 +1,13 @@
+const AddComments = () => {
+  return (
+    <div className="Input_box">
+      <from>
+        <label>Oceń i skomentuj</label>
+        <input type="text"></input>
+        <input type="submit" value="Submit"></input>
+      </from>
+    </div>
+  );
+};
+
+export default AddComments;
---
+++ src/components/BeerCardComments.js
@@ -0,0 +1,17 @@
+import "./BeerCardComments.css";
+
+const BeerCardComments = () => {
+  return (
+    <div className="Comments-box">
+      <ul>
+        <li>zajebiste piwo</li>
+        <li>zajebiste piwo</li>
+        <li>zajebiste piwo</li>
+        <li>zajebiste piwo</li>
+        <li>zajebiste piwo</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BeerCardComments;
