ed54de78548e97a3b0b02a5df278d6a9e28c40bb
jozef-wolf <jozefwolf@o2.pl>
2021-02-23 19:58:41 +0100

    Submit button changed

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -4,9 +4,7 @@ const AddComments = () => {
   return (
     <div className="Input_box">
       <form className="Input_form">
-        <label>
-          <h3>Oceń i skomentuj</h3>
-        </label>
+        <h3>Oceń i skomentuj</h3>
         <RatingStars />
         <textarea></textarea>
         <input className="Input_submit" type="submit" value="Submit"></input>
