709677fe2d739879bd295e81e7dc2990bc7ed39a
jozef-wolf <jozefwolf@o2.pl>
2021-02-23 18:49:23 +0100

    RatingStars div removed

--- src/components/RatingStars.js
+++ src/components/RatingStars.js
@@ -2,20 +2,18 @@ import React from "react";
 import { FaStar } from "react-icons/fa";
 const RatingStars = () => {
   return (
-    <div>
-      <label>
-        <input type="radio" name="rating" value="1" />
-        <FaStar />
-        <input type="radio" name="rating" value="2" />
-        <FaStar />
-        <input type="radio" name="rating" value="3" />
-        <FaStar />
-        <input type="radio" name="rating" value="4" />
-        <FaStar />
-        <input type="radio" name="rating" value="5" />
-        <FaStar />
-      </label>
-    </div>
+    <label>
+      <input type="radio" name="rating" value="1" />
+      <FaStar />
+      <input type="radio" name="rating" value="2" />
+      <FaStar />
+      <input type="radio" name="rating" value="3" />
+      <FaStar />
+      <input type="radio" name="rating" value="4" />
+      <FaStar />
+      <input type="radio" name="rating" value="5" />
+      <FaStar />
+    </label>
   );
 };
 
