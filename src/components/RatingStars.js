ee022e46ad35603a69ae63c4cdf26e02d01faff4
jozef-wolf <jozefwolf@o2.pl>
2021-02-23 18:31:06 +0100

    RatingStars added value

--- src/components/RatingStars.js
+++ src/components/RatingStars.js
@@ -3,9 +3,18 @@ import { FaStar } from "react-icons/fa";
 const RatingStars = () => {
   return (
     <div>
-      {[...Array(5)].map((star) => {
-        return <FaStar />;
-      })}
+      <label>
+        <input type="radio" name="rating" value="1" />
+        <FaStar />
+        <input type="radio" name="rating" value="2" />
+        <FaStar />
+        <input type="radio" name="rating" value="3" />
+        <FaStar />
+        <input type="radio" name="rating" value="4" />
+        <FaStar />
+        <input type="radio" name="rating" value="5" />
+        <FaStar />
+      </label>
     </div>
   );
 };
