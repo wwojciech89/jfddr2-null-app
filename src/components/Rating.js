6499b6986452326be0de978aa093c2666ccee3d3
jozef-wolf <jozefwolf@o2.pl>
2021-02-25 18:35:32 +0100

    rating with stars commented out

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,29 +1,27 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
-// import { useState } from "react";
+import { useState } from "react";
 
-const Rating = ({ beers }) => {
+const Rating = () => {
   //average
   //average to setrate
-
-  const RateArray = [4, 1, 5, 5];
+  const RateArray = [4, 3, 3, 5];
 
   function average(nums) {
     return (nums.reduce((a, b) => a + b) / nums.length).toFixed(0);
   }
   let Rate = average(RateArray);
-
-  // setRating(Rate);
+  // // setRating(Rate);
   console.log(Rate);
   return (
     <div className="Rating_stars">
-      {[...Array(5)].map((star, i) => {
+      {/* {[...Array(5)].map((star, i) => {
         return (
           <label>
-            <FaStar size={20} />
+            <FaStar size={20} color={Rate > 0 ? "grey" : "red"} />
           </label>
         );
-      })}
+      })} */}
       <p className="Rating_number">{Rate}/5</p>
     </div>
   );
