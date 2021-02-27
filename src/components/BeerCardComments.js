b47b6dcf9950880a06aae551d12758592673b046
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2021-02-27 09:20:37 +0100

    added working render comments at beerCard

--- src/components/BeerCardComments.js
+++ src/components/BeerCardComments.js
@@ -4,29 +4,34 @@ const BeerCardComments = ({ beers, id }) => {
   let filteredBeers;
   let filterdCommentary = [];
 
-  function elo() {
+  function renderComments() {
     filteredBeers = beers.filter((el) => {
       return el.id === id;
     });
     filteredBeers[0].commentary.map((el) => {
       console.log(el);
       filterdCommentary.push(el);
+      console.log(filterdCommentary);
     });
-    filterdCommentary.map((el) => {
+    const mapped = filterdCommentary.map((el) => {
       return (
-        <div className="commentary__container">
-          <p className="commentary__login">{el.login}</p>
-          <p className="commentary__text">{el.text}</p>
-        </div>
+        <li>
+          <div className="commentary__container">
+            <p className="commentary__login">{el.login}</p>
+            <p className="commentary__text">{el.text}</p>
+          </div>
+        </li>
       );
     });
+
+    return mapped;
   }
 
   return (
     <div className="Comments-box">
       <ul>
         <h3>Komentarze</h3>
-        {elo()}
+        {renderComments()}
       </ul>
     </div>
   );
