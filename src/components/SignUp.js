541203352f1875cbe37fc54b6e68a88c33e5961e
Tomasz Kopec <tomasz@artsound.pl>
2021-03-01 11:58:17 +0100

    improve home button, hide serach bar in some cases

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,14 +2,7 @@ import firebase from "../Firebase/firebase.js";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./login.css";
-
-const HomeButton = () => {
-  return (
-    <Link to={`/`}>
-      <button>HOME</button>
-    </Link>
-  );
-};
+import HomeButton from "./HomeButton";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -82,16 +75,16 @@ const SignUp = () => {
         </label>
 
         <input
-          className="formInput"
+          className="login__submit formInput"
           type="submit"
           value="Zarejestruj mnie"
         ></input>
+        <Link to={`/login`}>
+          <button className="signForm__button2">
+            Masz konto? Zaloguj się!
+          </button>
+        </Link>
       </form>
-      <Link to={`/login`}>
-        <button>
-          Masz konto? <br></br>Zaloguj się!
-        </button>
-      </Link>
     </div>
   );
 };
